66db628249b77a7f11ee64c6d45a8ad75561c2b9
MUJICAM <mujicam_9@hotmail.com>
2015-03-04 23:42:44 -0430

    df

--- src/GeneradorGrafico/Generar2.java
+++ src/GeneradorGrafico/Generar2.java
@@ -212,7 +212,7 @@ public class Generar2 extends JFrame {
     }//GEN-LAST:event_BotonPausaActionPerformed
 
     private void BotonIniciarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BotonIniciarMouseClicked
-            //miguel es pato
+            //miguel es pato y la tuya tambn
     }//GEN-LAST:event_BotonIniciarMouseClicked
 //metodos
 
